03176c07445b0fbf0a1c45e7ccedca0e8cf412c1
deepali <deepaalipatil@gmail.com>
2021-12-23 01:42:26 +0530

    UPDATED: gambler get  when win and loose  when loose

--- src/com/bridgelabz/Gambler.java
+++ src/com/bridgelabz/Gambler.java
@@ -4,5 +4,19 @@ public class Gambler {
 	public static void main(String[] args) {
 	    int stake = 100;
 	    int  everydayBet = 1;
+	    
+	    int condition = ((int) Math.floor(Math.random()*10)% 2);
+	      if ( condition == 1  ) {
+	    	stake = stake + everydayBet;
+	    	System.out.println("$" + stake + " stake");
+	    	System.out.println("Gambler win");
+	    }
+	    else {
+	    	 stake = stake - everydayBet;
+	    	 System.out.println("$"+ stake + " stake");
+	    	 System.out.println("Gambler loose");
+	    }
    }
+   
+   
 }
